008b20eb25640c39284deeb0d1c5d0af58393e48
anthoine <anthoine@etu.u-pec.fr>
2020-09-25 11:47:22 +0200

    reprise du projet !

--- chess.js
+++ chess.js
@@ -4,20 +4,33 @@ var plateau;
 var plateau_de_jeu;
 var couleur;
 var tr;
+var choix_piece;
 
-
+var checker = null;
 var highlight_checker = function (){
-    var checker;
     if(plateau_de_jeu.couleur === 1){
         checker = plateau_de_jeu.get_checker(1);
+        checker.push([plateau_de_jeu.roi_noir.la_case.x,plateau_de_jeu.roi_noir.la_case.y]);
     }else{
         checker = plateau_de_jeu.get_checker(0);
+        checker.push([plateau_de_jeu.roi_blanc.la_case.x,plateau_de_jeu.roi_blanc.la_case.y]);
     }
-    console.log("voyons voir");
     for(let i=0 ; i<checker.length ; i++){
         tr[checker[i][0]][checker[i][1]].classList.replace('white', 'light_red');
         tr[checker[i][0]][checker[i][1]].classList.replace('black', 'dark_red');
-        console.log(checker[i]);
+        tr[checker[i][0]][checker[i][1]].classList.replace('light_green', 'light_red');
+        tr[checker[i][0]][checker[i][1]].classList.replace('dark_green', 'dark_red');
+        tr[checker[i][0]][checker[i][1]].classList.replace('light_yellow', 'light_red');
+        tr[checker[i][0]][checker[i][1]].classList.replace('dark_yellow', 'dark_red');
+    }
+}
+var hide_checker = function (){
+    if(checker !== null){
+        for(let i=0 ; i<checker.length ; i++){
+            tr[checker[i][0]][checker[i][1]].classList.replace('light_red', 'white');
+            tr[checker[i][0]][checker[i][1]].classList.replace('dark_red', 'black');
+        }
+        checker = null;
     }
 }
 
@@ -64,6 +77,83 @@ var hide_case = function (target) {
         }
     }
 }
+var move_piece = function(tr_depart,tr_arrive){
+    var x_start = tr_depart.parentNode.sectionRowIndex;
+    var y_start = tr_depart.cellIndex;
+    var x_final = tr_arrive.parentNode.sectionRowIndex;
+    var y_final = tr_arrive.cellIndex;
+    var return_code;
+
+    if(couleur === 0){
+        if(((x_final===7 && plateau_de_jeu.tab_cases[7-x_final][7-x_final].piece.couleur===0)
+            || (x_final===0 && plateau_de_jeu.tab_cases[7-x_final][7-x_final].piece.couleur===1))
+            && plateau_de_jeu.tab_cases[7-x_start][7-y_start].piece.code === 0){
+            choose_piece(tr_depart,tr_arrive,7-x_start,7-y_start,7-x_final,7-y_final);
+            return false;
+        }
+        return_code = plateau_de_jeu.bouger_Piece(7-x_start, 7-y_start, 7-x_final, 7-y_final);
+    }else {
+        if((x_start===7 && plateau_de_jeu.tab_cases[x_start][y_start].piece.couleur===0)
+            || (x_final===0 && plateau_de_jeu.tab_cases[x_start][y_start].piece.couleur===0)
+            && plateau_de_jeu.tab_cases[7-x_start][7-y_start].piece.code === 0){
+            choose_piece(tr_depart,tr_arrive,x_start,y_start,x_final,y_final);
+            return false;
+        }
+        return_code = plateau_de_jeu.bouger_Piece(x_start, y_start, x_final, y_final);
+    }
+    if(return_code>=0){
+        hide_checker();
+        tr_arrive.removeChild(tr_arrive.lastElementChild);
+        tr_arrive.appendChild(tr_depart.lastElementChild);
+        tr_depart.innerHTML= "<img class='hide'  draggable='true' ondragstart='event.dataTransfer.setData(\"text/plain\",null)'>";
+        if(return_code === 2 || return_code === 3){
+            highlight_checker();
+        }
+        if(plateau_de_jeu.jeu_fini===1){
+            state_of_game.innerText = "Échec et mat";
+        }else if(plateau_de_jeu.jeu_fini ===2){
+            state_of_game.innerText = "Égalité";
+        }
+    }else if(return_code === -2){
+        state_of_game.innerText = "C'est pas ton tour !";
+    }
+}
+var choose_piece = function (tr_depart,tr_arrive, x_start, y_start, x_final, y_final){
+    tr_arrive.appendChild(choix_piece);
+    var new_plateau = plateau.cloneNode(true);
+    document.getElementById("container").replaceChild(new_plateau, plateau);
+    choix_piece = document.getElementById("choix_piece");
+
+    choix_piece.addEventListener('click', function (e){
+        if(e.target.nodeName === "IMG"){
+            document.getElementById("container").replaceChild(plateau, new_plateau);
+            choix_piece = document.getElementById("choix_piece");
+            choix_piece.parentNode.removeChild(choix_piece);
+
+            tr_depart.lastElementChild.src = e.target.src;
+            var return_code = plateau_de_jeu.bouger_Piece(x_start, y_start, x_final, y_final, parseInt(e.target.classList[0]));
+
+            if(return_code>=0){
+                hide_checker();
+                tr_arrive.removeChild(tr_arrive.lastElementChild);
+                tr_arrive.appendChild(tr_depart.lastElementChild);
+                tr_depart.innerHTML= "<img class='hide'  draggable='true' ondragstart='event.dataTransfer.setData(\"text/plain\",null)'>";
+                if(return_code === 2 || return_code === 3){
+                    highlight_checker();
+                }
+                if(plateau_de_jeu.jeu_fini===1){
+                    state_of_game.innerText = "Échec et mat";
+                }else if(plateau_de_jeu.jeu_fini ===2){
+                    state_of_game.innerText = "Égalité";
+                }
+            }else if(return_code === -2){
+                state_of_game.innerText = "C'est pas ton tour !";
+            }else{
+                console.log("erreur");
+            }
+        }
+    });
+}
 var click_plateau = function (e){
     var target;
     if(e.target.nodeName === 'IMG'){
@@ -82,34 +172,7 @@ var click_plateau = function (e){
     }else if(precedent_target !== target){
         if(target.classList.contains("dropzone")){
             hide_case(precedent_target);
-            var x_start = precedent_target.parentNode.sectionRowIndex;
-            var y_start = precedent_target.cellIndex;
-            var x_final = target.parentNode.sectionRowIndex;
-            var y_final = target.cellIndex;
-            var return_code;
-            if(couleur === 0){
-                return_code = plateau_de_jeu.bouger_Piece(7-x_start, 7-y_start, 7-x_final, 7-y_final);
-            }else {
-                return_code = plateau_de_jeu.bouger_Piece(x_start, y_start, x_final, y_final);
-            }
-            if(return_code>=0){
-                target.removeChild(target.lastElementChild);
-                target.appendChild(precedent_target.lastElementChild);
-                precedent_target.innerHTML= "<img class='hide'  draggable='true' ondragstart='event.dataTransfer.setData(\"text/plain\",null)'>";
-                if(return_code === 2 || return_code === 3){
-                    highlight_checker();
-                    if(plateau_de_jeu.jeu_fini===1){
-                        console.log("echec et mat");
-                        state_of_game.innerText = "Échec et mat";
-                    }else if(plateau_de_jeu.jeu_fini ===2){
-                        console.log("égalité");
-                        state_of_game.innerText = "Égalité";
-                    }
-                }
-            }else if(return_code === -2){
-                state_of_game.innerText = "C'est pas ton tour !";
-            }
-            console.log(return_code);
+            move_piece(precedent_target,target);
             precedent_target = null;
         }else{
             hide_case(precedent_target);
@@ -218,20 +281,7 @@ var drag_event_setup = function(){
             return false;
         }
         if (target.classList.contains("dropzone")) {
-            var x_final = target.parentNode.sectionRowIndex;
-            var y_final = target.cellIndex;
-            var return_code;
-            if(couleur === 0){
-                return_code = plateau_de_jeu.bouger_Piece(7-x_drag, 7-y_drag, 7-x_final, 7-y_final);
-            }else {
-                return_code = plateau_de_jeu.bouger_Piece(x_drag, y_drag, x_final, y_final);
-            }
-            if(return_code>=0){
-                console.log("coup autorisé");
-                dragged.parentNode.innerHTML= "<img class='hide'  draggable='true' ondragstart='event.dataTransfer.setData(\"text/plain\",null)'>";
-                target.removeChild(target.lastElementChild);
-                target.appendChild(dragged);
-            }
+            move_piece(dragged_parent,target)
 
             target.classList.replace('dark_yellow', 'dark_green');
             target.classList.replace('light_yellow', 'light_green');
@@ -239,10 +289,12 @@ var drag_event_setup = function(){
     }, false);
 }
 window.onload = function (){
+    choix_piece = document.getElementById("choix_piece");
+    choix_piece.parentNode.removeChild(choix_piece);
     state_of_game = document.getElementById("state_of_game");
     plateau_de_jeu = new Plateau();
     plateau_de_jeu.init();
-    couleur = 0;
+    couleur = 1;
 
     plateau = document.getElementById("plateau");
     var tbody = plateau.lastElementChild.children;
@@ -290,8 +342,6 @@ window.onload = function (){
     plateau.addEventListener('click',(e) => this.click_plateau(e), false);
 
     this.drag_event_setup();
-
-
 }
 function Plateau(){
     this.last_checker=[];
@@ -361,36 +411,49 @@ function Plateau(){
         }
         return [];
     }
-    /*-3 = pas de pièce à bouger
+    /* x,y : position de la pièce à déplacer
+    * a,b : position où déplacer la pièce
+    *  -3 = pas de pièce à bouger
     * -2 = pas le tour de la personne
     * -1 = move demandé impossible pour la piece
     *  0 = move made
     *  1 = move made and killed a piece
     *  2 = move made and check
     *  3 = move made and check and killed a piece*/
-    this.bouger_Piece = function (x, y, a, b){
+    this.bouger_Piece = function (x, y, a, b, piece_code){
         if(x<0 || x>=8 || y<0 || y>=8 || a<0 || a>=8 || b<0 || b>=8){
             console.log("out of bound");
             return false;
         }
         var return_code = -1;
-        if(this.tab_cases[x][y].piece !== null){
-            if(this.tab_cases[x][y].piece.couleur === this.couleur){
-                var cases = this.tab_cases[x][y].piece.cases_visitables;
+        if(this.tab_cases[x][y].piece !== null){ // y'a t'il une piece à déplacer ?
+            if(this.tab_cases[x][y].piece.couleur === this.couleur){//est-ce au tour de la piece de bouger ?
+                var cases = this.tab_cases[x][y].piece.cases_visitables;//quels coups lui sont autorisé
                 for(let i=0 ; i<cases.length ; i++){
-                    if(cases[i] === this.tab_cases[a][b]){
-                        //c'est bon la cases est valable
-                        if(this.tab_cases[a][b].piece === null){
-                            //personne à manger, déplacement simple
+                    if(cases[i] === this.tab_cases[a][b]){//c'est bon la cases est valable
+                        if(this.tab_cases[a][b].piece === null){//personne à manger, déplacement simple
                             return_code = 0;
-                        }else{
-                            //quelqu'un va se faire manger
+                        }else{//quelqu'un va se faire manger
                             return_code = 1;
                         }
+                        if(((a===7 && this.couleur===0) || (a===0 && this.couleur===1)) && this.tab_cases[x][y].piece.code===0){
+                            console.log("promotion");
+                            if(piece_code === 1){//Tour
+                                this.tab_cases[x][y].piece = new Tour(this.couleur,this.tab_cases[x][y]);
+                            }else if(piece_code ===2){//Cavalier
+                                this.tab_cases[x][y].piece = new Cavalier(this.couleur,this.tab_cases[x][y]);
+                            }else if(piece_code===3){//Fou
+                                this.tab_cases[x][y].piece = new Fou(this.couleur,this.tab_cases[x][y]);
+                            }else if(piece_code===4){//Reine
+                                this.tab_cases[x][y].piece = new Reine(this.couleur,this.tab_cases[x][y]);
+                            }
+                        }
+
                         this.tab_cases[a][b].piece = this.tab_cases[x][y].piece;
                         this.tab_cases[x][y].piece = null;
                         this.tab_cases[a][b].piece.la_case = this.tab_cases[a][b];
                         this.tab_cases[a][b].piece.hasMoved = true;
+
                         if(this.couleur === 0){
                             this.couleur = 1;
                         }else{
@@ -398,11 +461,9 @@ function Plateau(){
                         }
                         if(this.estEnEchec()){
                             this.plateau_en_echec = true;
-                            if(return_code === 1){
-                                //quelqu'un a été mangé et on a mis l'ennemi en echec
+                            if(return_code === 1){//quelqu'un a été mangé et on a mis l'ennemi en echec
                                 return_code = 3;
-                            }else{
-                                //juste mise en echec de l'ennemi
+                            }else{//juste mise en echec de l'ennemi
                                 return_code = 2;
                             }
                         }else{
@@ -426,24 +487,34 @@ function Plateau(){
         for(let i=0 ; i<8 ; i++){
             for(let j=0 ; j<8 ; j++){
                 if(this.tab_cases[i][j].piece !== null){
-                    var cases_visitables = this.tab_cases[i][j].piece.cases_reachable(this.tab_cases);
-                    for(let k=0 ; k<cases_visitables.length ; k++){
-                        var old_piece = cases_visitables[k].piece;
-                        cases_visitables[k].piece = this.tab_cases[i][j].piece
-                        this.tab_cases[i][j].piece = null;
-                        if(this.estEnEchec()){
-                            this.tab_cases[i][j].piece = cases_visitables[k].piece;
-                            cases_visitables[k].piece = old_piece;
-                            cases_visitables.splice(k, 1);
-                            k--;
-                        }else{
-                            this.tab_cases[i][j].piece = cases_visitables[k].piece;
-                            cases_visitables[k].piece = old_piece;
+                    if(this.tab_cases[i][j].piece.couleur === this.couleur){
+                        var cases_visitables = this.tab_cases[i][j].piece.cases_reachable(this.tab_cases);
+                        for(let k=0 ; k<cases_visitables.length ; k++){
+                            var old_piece = cases_visitables[k].piece;
+                            cases_visitables[k].piece = this.tab_cases[i][j].piece
+                            //added
+                            cases_visitables[k].piece.la_case = cases_visitables[k];
+                            this.tab_cases[i][j].piece = null;
+                            if(this.estEnEchec()){
+                                this.tab_cases[i][j].piece = cases_visitables[k].piece;
+                                cases_visitables[k].piece = old_piece;
+                                //added
+                                this.tab_cases[i][j].piece.la_case = this.tab_cases[i][j];
+                                cases_visitables.splice(k, 1);
+                                k--;
+                            }else{
+                                this.tab_cases[i][j].piece = cases_visitables[k].piece;
+                                cases_visitables[k].piece = old_piece;
+                                //added
+                                this.tab_cases[i][j].piece.la_case = this.tab_cases[i][j];
+                            }
                         }
-                    }
-                    this.tab_cases[i][j].piece.cases_visitables = cases_visitables;
-                    if(cases_visitables.length>0){
-                        pas_de_coup_dispo = false;
+                        this.tab_cases[i][j].piece.cases_visitables = cases_visitables;
+                        if(cases_visitables.length>0){
+                            pas_de_coup_dispo = false;
+                        }
+                    }else{
+                        this.tab_cases[i][j].piece.cases_visitables = [];
                     }
                 }
             }
@@ -452,7 +523,59 @@ function Plateau(){
             if(this.plateau_en_echec){
                 this.jeu_fini = 1;
             }else{
+                console.log("pat");
                 this.jeu_fini = 2;
+            }
+        }
+    }
+    this.check_roque = function () {
+        if (!this.plateau_en_echec) {
+            if (this.couleur === 0) {
+                if (!this.roi_blanc.hasMoved) {
+                    for (let i = 0; i < this.roi_blanc.cases_visitables; i++) {
+                        if (this.tab_cases[0][2] === this.roi_blanc.cases_visitables[i]) {
+                            if (this.tab_cases[0][0].piece !== null) {
+                                if (this.tab_cases[0][0].piece.code === 1 && !this.tab_cases[0][0].piece.hasMoved) {
+                                    if (this.tab_cases[0][1].piece === null && this.tab_cases[0][2].piece === null) {
+                                        this.roi_blanc.cases_visitables.push(this.tab_cases[0][1]);
+                                    }
+                                }
+                            }
+                        }
+                        if (this.tab_cases[0][4] === this.roi_blanc.cases_visitables[i]) {
+                            if (this.tab_cases[0][7].piece !== null) {
+                                if (this.tab_cases[0][7].piece.code === 1 && !this.tab_cases[0][7].piece.hasMoved) {
+                                    if (this.tab_cases[0][6].piece === null && this.tab_cases[0][5].piece === null && this.tab_cases[0][4].piece === null) {
+                                        this.roi_blanc.cases_visitables.push(this.tab_cases[0][5]);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }else if (this.couleur === 1) {
+                if (!this.roi_noir.hasMoved) {
+                    for (let i = 0; i < this.roi_noir.cases_visitables; i++) {
+                        if (this.tab_cases[7][2] === this.roi_noir.cases_visitables[i]) {
+                            if (this.tab_cases[7][0].piece !== null) {
+                                if (this.tab_cases[7][0].piece.code === 1 && !this.tab_cases[7][0].piece.hasMoved) {
+                                    if (this.tab_cases[7][1].piece === null && this.tab_cases[7][2].piece === null) {
+                                        this.roi_noir.cases_visitables.push(this.tab_cases[7][1]);
+                                    }
+                                }
+                            }
+                        }
+                        if (this.tab_cases[7][4] === this.roi_noir.cases_visitables[i]) {
+                            if (this.tab_cases[7][7].piece !== null) {
+                                if (this.tab_cases[7][7].piece.code === 1 && !this.tab_cases[7][7].piece.hasMoved) {
+                                    if (this.tab_cases[7][6].piece === null && this.tab_cases[7][5].piece === null && this.tab_cases[7][4].piece === null) {
+                                        this.roi_noir.cases_visitables.push(this.tab_cases[7][5]);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
--- index.php
+++ index.php
@@ -8,13 +8,13 @@
 </head>
 <body>
     <h1>Chess Time Boys !</h1>
-    <div class="container">
+    <div id="container">
         <table id="plateau">
             <tbody>
                 <?php for($i=0 ; $i<8 ; $i++){
                     echo("<tr>");
                     for($j=0 ; $j<8 ; $j++){
-                        if(($j+$i)%2==0){
+                        if(($j+$i)%2==1){
                             echo("<td class='not_active black hide'>");
                         }else {
                             echo("<td class='not_active white hide'>");
@@ -25,6 +25,11 @@
                 } ?>
             </tbody>
         </table>
+        <div id="choix_piece">
+            <div class="center">
+                <img class="2" src="img/cavalier_blanc.png"><img class="3" src="img/fou_blanc.png"><img class="1" src="img/tour_blanc.png"><img class="4" src="img/reine_blanc.png">
+            </div>
+        </div>
     </div>
 
     <h2 id="state_of_game"></h2>
